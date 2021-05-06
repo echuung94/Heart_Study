c50a415dedde43158cacf9f3595a8773bc69ead6
Nguyen Cao <nguyen.cao247@hotmail.com>
2021-05-05 23:44:33 -0400

    updating for seg 2

--- schema.sql
+++ schema.sql
@@ -1,8 +1,39 @@
--- Creating tables for PH-EmployeeDB
-CREATE TABLE departments (
-     dept_no VARCHAR(4) NOT NULL,
-     dept_name VARCHAR(40) NOT NULL,
-     PRIMARY KEY (dept_no),
-     UNIQUE (dept_name)
+-- Creating tables for heart_study db
+CREATE TABLE Heart_Disease (
+    male INT,
+    age	INT,
+    education INT,
+    currentSmoker INT,
+    cigsPerDay INT,
+    BPMeds INT,
+    prevalentStroke	INT,
+    prevalentHyp INT,
+    diabetes INT,
+    totChol INT,
+    sysBP INT,
+    diaBP INT,
+    BMI INT,
+    heartRate INT,
+    glucose INT,
+    TenYearCHD INT
 );
 
+CREATE TABLE Heart_Attack (
+    age	INT,
+    sex	INT,
+    cp INT,
+    trtbps INT,
+    chol INT,
+    fbs	INT,
+    restecg	INT,
+    thalachh INT,
+    exng INT,
+    oldpeak	INT,
+    slp	INT,
+    caa	INT,
+    thall INT,
+    output INT
+);
+
+SELECT * FROM Heart_Disease;
+SELECT * FROM Heart_Attack;
